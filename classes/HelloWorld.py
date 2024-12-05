23b4628c014df67026e1a9a4555dd839b2a2ca7e
Vincent BENOIT <vincent.benoit@inserm.fr>
2024-12-05 12:27:42 +0100

    Hello World

---
+++ classes/HelloWorld.py
@@ -0,0 +1,8 @@
+class HelloWorld:  
+
+    def __init__(self):
+        self.data = []
+
+
+    def greet(self):
+        print("Hello World")
---
+++ eddi4py.py
@@ -0,0 +1,8 @@
+from classes.HelloWorld import HelloWorld
+
+def main():
+    x=HelloWorld()
+    x.greet()
+
+if __name__ == '__main__':
+    main()
