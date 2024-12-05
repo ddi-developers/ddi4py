8b3ff24f46f4e5fff78c3a5b8407a068abec9967
Vincent BENOIT <vincent.benoit@inserm.fr>
2024-12-05 14:33:23 +0100

    py files with comments

--- generate_classes.py
+++ generate_classes.py
@@ -23,12 +23,22 @@ for k,v in ddi_property['patternProperties'].items():
   
 ### write files
 for property_name, property_attr in ddi_json["properties"].items():
+    
+    ddi_property = ddi_json['properties'][property_name]
+    code_class_comment = '"""'+"\n"+json.dumps(property_attr, indent=4)+"\n"+'"""'
+  
+
+
+
     code_class_str = f"""
+
 class {property_name}:
     
     def __init__(self):
         self = self
     """
     class_file = open(f"classes/{property_name}.py", "w+")
+    class_file.write(code_class_comment)
     class_file.write(code_class_str)
+    
     class_file.close()
