7738be9baff5b5e898ab0adc06ffd1afc918bf67
Vincent BENOIT <vincent.benoit@inserm.fr>
2024-12-05 14:12:56 +0100

    iterate properties

--- generate_classes.py
+++ generate_classes.py
@@ -1,3 +1,23 @@
 import json
 import yaml
 
+
+
+##
+# VBE
+##
+
+with open('config.yaml', 'r') as file:
+ config = yaml.safe_load(file)
+
+
+with open(config['input_path']+"/"+config['ddi4_cogs_json']) as f:
+    ddi_json = json.load(f)
+
+ddi_property = ddi_json['properties']['studyUnit']
+
+#print(json.dumps(ddi_property['patternProperties'], indent=4))
+
+for k,v in ddi_property['patternProperties'].items():
+   print(k+" | "+str(v))
+   print(json.dumps(v['properties'], indent=4))
