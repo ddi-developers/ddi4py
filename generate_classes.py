b0d1b442889545643a26b498abc14cb807e157bc
Vincent BENOIT <vincent.benoit@inserm.fr>
2024-12-05 14:58:07 +0100

    change of config and paths

--- generate_classes.py
+++ generate_classes.py
@@ -28,7 +28,8 @@ for property_name, property_attr in ddi_json["properties"].items():
     code_class_comment = '"""'+"\n"+json.dumps(property_attr, indent=4)+"\n"+'"""'
   
 
-
+    property_filename=f"./{config['properties_path']}/{property_name}.py"
+    print(property_filename)
 
     code_class_str = f"""
 
@@ -37,7 +38,7 @@ class {property_name}:
     def __init__(self):
         self = self
     """
-    class_file = open(f"classes/{property_name}.py", "w+")
+    class_file = open(property_filename, "w+")
     class_file.write(code_class_comment)
     class_file.write(code_class_str)
     
