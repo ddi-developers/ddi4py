5f64781111d6380ce46c82fcd34b2ce2064fce98
Vincent BENOIT <vincent.benoit@inserm.fr>
2024-12-05 15:05:36 +0100

    cleanup

--- generate_classes.py
+++ generate_classes.py
@@ -13,14 +13,7 @@ with open('config.yaml', 'r') as file:
 with open(config['input_path']+"/"+config['ddi4_cogs_json']) as f:
     ddi_json = json.load(f)
 
-ddi_property = ddi_json['properties']['studyUnit']
 
-#print(json.dumps(ddi_property['patternProperties'], indent=4))
-
-for k,v in ddi_property['patternProperties'].items():
-   print(k+" | "+str(v))
-   print(json.dumps(v['properties'], indent=4))
-  
 ### write files
 for property_name, property_attr in ddi_json["properties"].items():
     
