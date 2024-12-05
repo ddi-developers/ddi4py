905ad608615bb51b8c8d5e83290aa46d2ec1e871
Thibaud Ritzenthaler <thibaud.Ritzenthaler@pm.me>
2024-12-05 14:07:47 +0100

    :sparkles: create class files

--- generate_classes.py
+++ generate_classes.py
@@ -1,3 +1,17 @@
 import json
 import yaml
 
+with open('./input/jsonSchema.json', 'r') as file:
+    ddi_l = json.load(file)
+
+for property_name, property_attr in ddi_l["properties"].items():
+    code_class_str = f"""
+class {property_name}:
+    
+    def __init__(self):
+        self = self
+    """
+    class_file = open(f"classes/{property_name}.py", "w+")
+    class_file.write(code_class_str)
+    class_file.close()
+
